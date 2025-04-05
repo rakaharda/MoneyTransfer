423f0b68f16ba108c52bc5e912001818a4872cc8
rakaharda <dnsprikhodko@gmail.com>
2025-04-04 17:51:48 -0700

    add build artifacts into repo

---
+++ transfer-service/build/resources/main/sql/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS users (
+    username VARCHAR NOT NULL,
+    userid uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    balance FLOAT DEFAULT 0,
+    token uuid DEFAULT gen_random_uuid()
+);
+
+CREATE TABLE IF NOT EXISTS transfers (
+    transferid uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    transferdate timestamp with time zone DEFAULT now(),
+    senderid uuid NOT NULL,
+    recipientid uuid NOT NULL,
+    amount FLOAT NOT NULL
+);
